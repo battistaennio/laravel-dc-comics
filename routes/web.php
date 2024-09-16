afbda9aa64a794381f7970803675f1e25b1ffc38
battistaennio <ennio.battista99@gmail.com>
2024-09-16 16:22:39 +0200

    feat: create Comic controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ComicController;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\PageController;
@@ -18,3 +19,4 @@ use App\Http\Controllers\PageController;
 Route::get('/', [PageController::class, 'index'])->name('home');
 Route::get('/chi-siamo', [PageController::class, 'about'])->name('about');
 Route::get('/contatti', [PageController::class, 'contacts'])->name('contacts');
+Route::resource('comics', ComicController::class);
