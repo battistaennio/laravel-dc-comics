15fe279dbc15bba22c47ffe02d0c6e94555c3d5a
battistaennio <ennio.battista99@gmail.com>
2024-09-17 16:22:49 +0200

    feat: add delete comic

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -82,6 +82,9 @@ class ComicController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $comic = Comic::find($id);
+        $comic->delete();
+
+        return redirect()->route('comics.index')->with('delete_confirm', 'Comic "' . $comic->title . '" deleted successfully');
     }
 }
--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -2,6 +2,13 @@
 
 @section('content')
     <div class="container my-5">
+
+        @if (session('delete_confirm'))
+            <div class="alert alert-success" role="alert">
+                {{ session('delete_confirm') }}
+            </div>
+        @endif
+
         <h1>Comics List</h1>
 
         <table class="table">
@@ -24,13 +31,24 @@
                         <td>{{ $comic->type }}</td>
                         <td><img class="thumb" src="{{ $comic->thumb }}" alt="{{ $comic->title }}"></td>
                         <td>
-                            <a class="btn btn-warning" href="{{ route('comics.show', $comic) }}">
+                            {{-- dettaglio --}}
+                            <a class="btn btn-success" href="{{ route('comics.show', $comic) }}">
                                 <i class="fa-solid fa-magnifying-glass"></i>
                             </a>
-
+                            {{-- modifica --}}
                             <a class="btn btn-warning" href="{{ route('comics.edit', $comic) }}">
                                 <i class="fa-solid fa-pencil"></i>
                             </a>
+                            {{-- elimina --}}
+                            <form class="d-inline" action="{{ route('comics.destroy', $comic) }}" method="POST"
+                                onsubmit="return confirm('Are you sure you want to permanently delete this comic? All \'{{ $comic->title }}\' data will be lost.')">
+                                @csrf
+                                @method('DELETE')
+
+                                <button class="btn btn-danger" type="submit">
+                                    <i class="fa-solid fa-trash"></i>
+                                </button>
+                            </form>
 
                         </td>
                     </tr>
